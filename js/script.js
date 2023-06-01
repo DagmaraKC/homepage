e5064d4fb12a5ba757e8266203fc69d15650c441
Dagmara Kot-Czerzniewska <kotdagmara1@gmail.com>
2023-06-01 14:02:41 +0200

    added new function to JS file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-console.log("Witaj w moim kodzie :)");
+{
+    const welcome = () => {
+        console.log("Witaj w moim kodzie :)");
+    };
 
-let backgroundButton = document.querySelector(".js-backgroundButton");
-let lightBackground = document.querySelector(".js-lightBackground");
-let darkBackground = document.querySelector(".body__background--dark");
-let body = document.querySelector(".body");
-let button = document.querySelector(".js-backgroundButton");
-let navigation = document.querySelector(".js-navigation");
-let navigationDark = document.querySelector(".navigation__link--dark");
+    const OnChangeThemeClick = () => {
+        const backgroundButton = document.querySelector(".js-backgroundButton");
+        const navigation = document.querySelector(".js-navigation");
+        const body = document.querySelector(".body");
 
-backgroundButton.addEventListener("click", () => {
-    body.classList.toggle("body__background--dark");
-    navigation.classList.toggle("navigation__link--dark");
+        body.classList.toggle("body__background--dark");
+        navigation.classList.toggle("navigation__link--dark");
 
-    backgroundButton.innerText = body.classList.contains(
-        "body__background--dark"
-    )
-        ? "Wyłącz ciemny motyw"
-        : "Włącz ciemny motyw";
-});
+        backgroundButton.innerText = body.classList.contains(
+            "body__background--dark"
+        )
+            ? "Wyłącz ciemny motyw"
+            : "Włącz ciemny motyw";
+    };
+
+    const init = () => {
+        const backgroundButton = document.querySelector(".js-backgroundButton");
+        backgroundButton.addEventListener("click", OnChangeThemeClick);
+        welcome();
+    };
+
+    init();
+}
